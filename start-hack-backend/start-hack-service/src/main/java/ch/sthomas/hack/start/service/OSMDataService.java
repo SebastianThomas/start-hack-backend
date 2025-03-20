85b28d23c466069a9c5a593169a5b64171fcbb69
SebastianThomas <kontakt@sthomas.ch>
2025-03-20 21:20:40 +0100

    Add different OSM layers

--- start-hack-backend/start-hack-service/src/main/java/ch/sthomas/hack/start/service/OSMDataService.java
+++ start-hack-backend/start-hack-service/src/main/java/ch/sthomas/hack/start/service/OSMDataService.java
@@ -1,5 +1,6 @@
 package ch.sthomas.hack.start.service;
 
+import ch.sthomas.hack.start.model.feature.BaseFeature;
 import ch.sthomas.hack.start.model.feature.BaseFeatureCollection;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 @Service
 public class OSMDataService {
@@ -27,32 +29,45 @@ public class OSMDataService {
     }
 
     public void loadAndSaveData() throws IOException {
-        final var points =
-                new BaseFeatureCollection()
-                        .setFeatures(
-                                objectMapper
-                                        .readValue(
-                                                data.resolve("mauritania-points.geojson").toFile(),
-                                                BaseFeatureCollection.class)
-                                        .getFeatures()
-                                        .stream()
-                                        .filter(
-                                                f ->
-                                                        f.getProperties().values().stream()
-                                                                .anyMatch(Objects::nonNull))
-                                        .toList());
-        final var lines =
-                new BaseFeatureCollection()
-                        .setFeatures(
-                                objectMapper
-                                        .readValue(
-                                                data.resolve("mauritania-lines.geojson").toFile(),
-                                                BaseFeatureCollection.class)
-                                        .getFeatures()
-                                        .stream()
-                                        .filter(f -> f.getProperty("highway") != null)
-                                        .toList());
-        objectMapper.writeValue(outputFolder.resolve("assaba-points.geojson").toFile(), points);
+        final var points = loadFeatureCollection(data.resolve("mauritania-points.geojson"));
+        final var allPoints =
+                filterFeatureCollection(
+                        points,
+                        f -> f.getProperties().values().stream().anyMatch(Objects::nonNull));
+        final var manMadePoints = filterContainingKey(allPoints, "man_made");
+        objectMapper.writeValue(
+                outputFolder.resolve("assaba-all-points.geojson").toFile(), allPoints);
+        objectMapper.writeValue(
+                outputFolder.resolve("assaba-man-made-points.geojson").toFile(), manMadePoints);
+
+        final var lines = loadFeatureCollection(data.resolve("mauritania-lines.geojson"));
+        final var highways = filterContainingKey(lines, "highway");
+        final var waterways =
+                filterFeatureCollection(
+                        lines,
+                        f ->
+                                f.getProperties().containsKey("waterway")
+                                        || f.getProperties()
+                                                .getOrDefault("natural", "no")
+                                                .equals("water"));
         objectMapper.writeValue(outputFolder.resolve("assaba-lines.geojson").toFile(), lines);
+        objectMapper.writeValue(outputFolder.resolve("assaba-highways.geojson").toFile(), highways);
+        objectMapper.writeValue(
+                outputFolder.resolve("assaba-waterways.geojson").toFile(), waterways);
+    }
+
+    private BaseFeatureCollection filterContainingKey(
+            final BaseFeatureCollection featureCollection, final String key) {
+        return filterFeatureCollection(featureCollection, f -> f.getProperties().containsKey(key));
+    }
+
+    private BaseFeatureCollection filterFeatureCollection(
+            final BaseFeatureCollection featureCollection, final Predicate<BaseFeature> predicate) {
+        return new BaseFeatureCollection()
+                .setFeatures(featureCollection.getFeatures().stream().filter(predicate).toList());
+    }
+
+    private BaseFeatureCollection loadFeatureCollection(final Path path) throws IOException {
+        return objectMapper.readValue(path.toFile(), BaseFeatureCollection.class);
     }
 }
